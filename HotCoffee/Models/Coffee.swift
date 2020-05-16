9ee66ffc4ea4414a975279f1f76da1b72f79a079
Praveen <praveenislive@gmail.com>
2020-05-17 01:35:39 +0530

    Initial Commit with Project Setup

---
+++ HotCoffee/Models/Coffee.swift
@@ -0,0 +1,15 @@
+//
+//  Coffee.swift
+//  HotCoffee
+//
+//  Created by Praveen on 7/7/18.
+//  Copyright © 2018 Praveen. All rights reserved.
+//
+
+import Foundation
+
+struct Coffee {
+    var name :String
+    var imageURL :String
+    var basePrice :Double
+}
---
+++ HotCoffee/Models/Order.swift
@@ -0,0 +1,34 @@
+//
+//  Order.swift
+//  HotCoffee
+//
+//  Created by Praveen on 7/7/18.
+//  Copyright © 2018 Praveen. All rights reserved.
+//
+
+import Foundation
+
+enum CoffeeSize :Int {
+    case small = 0
+    case medium = 1
+    case large = 2
+}
+
+class Order {
+    
+    var identifier :UUID
+    var coffee :Coffee
+    var total :Double
+    var size :CoffeeSize
+    
+    init(coffee :Coffee, total :Double, size :CoffeeSize = .medium, identifier :UUID = UUID()) {
+        self.coffee = coffee
+        self.total = total
+        self.size = size
+        self.identifier = identifier
+    }
+}
+
+
+
+
